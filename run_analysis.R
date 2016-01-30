cc1db3d2610bebc35bdc0abab0ec80ab714b50de
knutux <knutux@gmail.com>
2016-01-30 19:53:30 +0200

    inittial commit

---
+++ run_analysis.R
@@ -0,0 +1,15 @@
+# A script which
+#  - looks for data collected from the accelerometers from the Samsung Galaxy S
+#    smartphone (it must be located in "UCI HAR Dataset" directory, which is
+#    created in current working directory )
+#  - Merges the training and the test sets to create one data set
+#  - Extracts the measurements on the mean and standard deviation for each measurement
+#  - Uses descriptive activity names to name the activities in the data set
+#  - Appropriately labels the data set with descriptive variable names
+#  - From the data set in step 4, creates a second, independent tidy data set
+#    with the average of each variable for each activity and each subject
+
+# Main function (script entry point)
+run_analysis <- function () {
+    
+}
